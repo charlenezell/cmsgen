70bf4f84636a98321f279efe95f9f352e5e2cee5
zellw <charlenezell@gmail.com>
2018-01-25 10:22:23 +0800

    httpokversion1

--- index.js
+++ index.js
@@ -1,60 +1,71 @@
 // import fetch from "isomorphic-fetch";
 import fetch from "node-fetch";
-const { URL } = require("url");
-
-// let token = "JSESSIONID=0A2E19D57D355FA73D096FB70C424743";
-let token = "";
-let cmsRoot = "http://e0d41ab0.ngrok.io/";
-// let cmsRoot = 'http://cmsservice.100bt.com/';
-
+import { URL, URLSearchParams } from "url";
+import proxyAgent from 'https-proxy-agent'
 import setCookie from "set-cookie-parser";
-import { URLSearchParams } from "url";
+
+
+const isDebug = true;
+
+const debugOption = {
+  agent: new proxyAgent("http://127.0.0.1:8123")
+}
+
+// let cmsRoot = "http://e0d41ab0.ngrok.io/";
+let cmsRoot = 'http://cmsservice.100bt.com/';
+
+
+async function goindex() {
+  return await fetch(cmsRoot, isDebug ? { ...debugOption } : {});
+}
 
 async function login(userName, password) {
   let url = `${cmsRoot}sysmanager/login.html`;
-  return await fetch(url, {
+  let param = new URLSearchParams({
+    userName,
+    password
+  })
+  let arg = {
     method: "POST", // or 'PUT'
-    body: {
-      userName,
-      password
-    },
+    body: param.toString(),
     headers: {
-      "Referer":"http://e0d41ab0.ngrok.io/sysmanager/login.html"
+      Cookie: token,
+      "Content-Type": "application/x-www-form-urlencoded"
     }
-  });
+  }
+  return await fetch(url, isDebug ? { ...debugOption, ...arg } : arg).then(v => v.text());
 }
 
 async function queryCategoryById(categoryId) {
   let url = new URL(`${cmsRoot}queryCategoryById.action`);
   url.searchParams.append("categoryId", categoryId);
-  return await fetch(url.toString(), {
+  let arg = {
     method: "GET",
     headers: {
       Cookie: token
     }
-  }).then(v => v.json());
+  }
+  // console.log(url);
+  return await fetch(url.toString(), isDebug ? { ...debugOption, ...arg } : arg).then(v => v.json());
 }
 
+
+let token = "";
+
 export default async function main() {
-  //   let c = await login("dev", "222222");
-
-  //   console.log(c);
-  //   console.log(setCookie(c.headers));
-  //   console.log(setCookie(c.headers.get("set-cookie")));
-  // return false;
-  if (!token) {
-    try {
-      let c = await login("dev", "222222");
-      let w = setCookie(c.headers.get("set-cookie"))[0];
-      console.log(c.headers.get("set-cookie"));
-      console.log(w);
-      token = [w.name, w.value].join("=");
-      console.log(token);
-    } catch (e) {
-      console.log(e);
-    }
+  try {
+    let c = await goindex();
+    let w = setCookie(c.headers.get("set-cookie"))[0];
+    token = [w.name, w.value].join("=");
+  } catch (e) {
+    throw e;
   }
-
+  try {
+    let c = await login("dev", "222222");
+    console.log(c);
+  } catch (e) {
+    console.log(e);
+  }
   try {
     let c = await queryCategoryById(20870);
     console.log(c);
